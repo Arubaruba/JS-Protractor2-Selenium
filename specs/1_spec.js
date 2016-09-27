eb66b1f2b0bd4db7ac574aca689724d271d4c5f1
Andreas Stocker <shadowstep7@gmail.com>
2016-09-27 20:27:30 +0200

    Update 1_spec.js

--- specs/1_spec.js
+++ specs/1_spec.js
@@ -3,9 +3,9 @@ describe('Protractor Demo App1', function() {
   it('should greet the named user', function() {
     browser.get('http://www.angularjs.org');
 
-    element(by.model('yourName')).sendKeys('Example');
+    element(by.model('yourName1234')).sendKeys('Example');
 
-    var greeting = element(by.binding('yourName'));
+    var greeting = element(by.binding('yourName1234'));
 
     expect(greeting.getText()).toEqual('Hello Example!');
   });
